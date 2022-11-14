5139dcaddc8e84362b4ce8bf5e728a0bc7398f39
Hank Kim <hank29206880@gmail.com>
2022-11-14 16:25:37 +0900

    optimistic update

--- src/components/RQHeros.jsx
+++ src/components/RQHeros.jsx
@@ -1,9 +1,14 @@
-import useSuperHeroesData from "../hooks/useSuperHeroesData";
+import useSuperHeroesData, {
+  useAddSuperHeroData,
+} from "../hooks/useSuperHeroesData";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const RQHeroes = () => {
   const [time, setTime] = useState(3000);
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+
   const onSuccess = (data) => {
     if (data.length > 3) {
       setTime(0);
@@ -15,6 +20,13 @@ export const RQHeroes = () => {
   };
   const { data, isLoading, isError, error, isFetching, refetch } =
     useSuperHeroesData(onSuccess, onError, time);
+
+  const { mutate: addHero } = useAddSuperHeroData();
+  const handleAddHeroClick = () => {
+    const hero = { name, alterEgo };
+    addHero(hero);
+  };
+
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
@@ -26,6 +38,19 @@ export const RQHeroes = () => {
   return (
     <>
       <h2>RQ Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        '<button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
       <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return (
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,14 +1,46 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:4000/superheroes", hero);
+};
+
 const useSuperHeroesData = (onSuccess, onError) => {
   return useQuery("heroes", fetchHeroes, {
     onSuccess,
     onError,
+  });
+};
+
+export const useAddSuperHeroData = () => {
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("heroes");
+      const previousHeroData = queryClient.getQueryData("heroes");
+      queryClient.setQueryData("heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
+        };
+      });
+      return {
+        previousHeroData,
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("heroes", context.previousHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("heroes");
+    },
   });
 };
 
